a5e911ba4e6541f06ea3e74021f889d78f8fa6c1
Marina Ivanova <ubcg62bs@Marinas-MacBook-Pro.local>
2022-08-30 12:03:08 +0100

    Dish class - category and show added

--- lib/dish.rb
+++ lib/dish.rb
@@ -13,4 +13,13 @@ class Dish
   def price
     @price
   end
+
+  def category(type)
+    @type = type.to_sym
+  end
+
+  def show(io = Kernel)
+    @io = io
+    @io.puts "#{@dish_name} - #{sprintf('%.2f', @price)}, category: #{@type}"
+  end
 end
--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Dish do
     expect(new_dish.dish_name).to eq "pasta"
     expect(new_dish.price).to eq 10.00
   end
+
+  it "adds the category to the dish" do
+    io = double :io
+    expect(io).to receive(:puts).with("pasta - 10.00, category: main")
+    new_dish = Dish.new("pasta", 10.00)
+    new_dish.category("main")
+    new_dish.show(io)
+  end
 end
